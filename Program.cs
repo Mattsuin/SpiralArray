5959caaa9899b26eb292aab93d85595851698dfe
Mattias Borgenstierna <m.borgenstierna@hotmail.com>
2024-09-23 16:11:48 +0200

    Add loop to iterate over array

--- Program.cs
+++ Program.cs
@@ -8,13 +8,29 @@
             double testSpiral = Math.Sqrt(arraySize);
             int spiralSize = (int)testSpiral;
 
-            //Check if array can form a square with equal sides
+            // Check if array can form a square with equal sides.
             if (testSpiral != spiralSize)
             {
                 throw new ArgumentException("Invalid array size");
             }
 
-            //Temp return statement
+            int x = 0, y = 0;
+            int stepCounter = 0;
+            int step = spiralSize - 1;
+
+            for (int i = 1; i < arraySize; i++)
+            {
+                // Do stuff first
+
+                stepCounter++;
+                if (stepCounter == step)
+                {
+                    stepCounter = 0;
+                    // Rotate direction.
+                }
+            }
+
+            // Temp return statement
             return array;
         }
 
@@ -23,7 +39,7 @@
             /*
              * Given the single-dimensional array [1,2,3,4,5,6,7,8,9] the array [1,2,3,8,9,4,7,6,5] is returned.
              */
-            int[] singleDimensionArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] singleDimensionArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
             Spiral(singleDimensionArray);
 
             /*
