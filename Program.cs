d657f20557e5cc20a92830fcf5e4bed184b9020c
Mattias Borgenstierna <m.borgenstierna@hotmail.com>
2024-09-23 14:29:31 +0200

    Add objectives to achieve

--- Program.cs
+++ Program.cs
@@ -1,9 +1,19 @@
 ﻿namespace SpiralArray
 {
-    internal class Program
+    public class SpiralArray
     {
         static void Main(string[] args)
         {
+            /*
+             * Given the single-dimensional array [1,2,3,4,5,6,7,8,9] the returned array should be [1,2,3,8,9,4,7,6,5].
+             */
+
+
+            /*
+             * Given the two dimensional array [[1,2,3], the returned array should be  [[1,2,3],
+             *                                  [4,5,6],                                [8,9,4],
+             *                                  [7,8,9]]                                [7,6,5]
+             */
         }
     }
 }
