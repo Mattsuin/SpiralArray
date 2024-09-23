8af5297ad0f679d5fc9842bcc6c32cca87f5e91d
Mattias Borgenstierna <m.borgenstierna@hotmail.com>
2024-09-23 15:28:12 +0200

    Reword the objectives

--- Program.cs
+++ Program.cs
@@ -21,15 +21,15 @@
         static void Main(string[] args)
         {
             /*
-             * Given the single-dimensional array [1,2,3,4,5,6,7,8,9] the returned array should be [1,2,3,8,9,4,7,6,5].
+             * Given the single-dimensional array [1,2,3,4,5,6,7,8,9] the array [1,2,3,8,9,4,7,6,5] is returned.
              */
             int[] singleDimensionArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             Spiral(singleDimensionArray);
 
             /*
-             * Given the two dimensional array [[1,2,3], the returned array should be  [[1,2,3],
-             *                                  [4,5,6],                                [8,9,4],
-             *                                  [7,8,9]]                                [7,6,5]
+             * Given the two dimensional array [[1,2,3], the array [[1,2,3], is returned.
+             *                                  [4,5,6],            [8,9,4],
+             *                                  [7,8,9]]            [7,6,5]
              */
         }
     }
